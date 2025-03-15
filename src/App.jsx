e968296b087a6d1941bdecffa849fba8fb47d278
ashish-abir <ashish.ranjan@abirnetworks.com>
2025-03-15 08:35:04 +0530

    first remote commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-    return (
-        <div>
-            app
-        </div>
-    )
-}
+    return <div>react concepts</div>;
+};
 
-export default App
+export default App;
